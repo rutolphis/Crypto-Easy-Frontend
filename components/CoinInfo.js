af2c0a252f2619e63907389d66be428b41d256c3
Rudolf420 <56067256+Rudolf420@users.noreply.github.com>
2021-04-14 17:30:50 +0200

    Added rates

--- components/CoinInfo.js
+++ components/CoinInfo.js
@@ -1,20 +1,133 @@
-import React from "react"
+import React, { useState , useEffect} from "react"
 import { View, Text, Image } from "react-native"
 
+
 const Coin = () => {
-    fetch("https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=1&limit=1&convert=EUR", {
-        method: "GET",
-        headers: {
-        Accept: "application/json",
-        'X-CMC_PRO_API_KEY' : '89491b09-0f1e-4585-80cf-a13097fdc682'
-        }
-    }).then((response) => response.json()).then((json) => {
-        console.log(json.data[0].quote.EUR.price)
-    })
+
+    const cryptoTemplate = {
+        data:[ {
+            name: undefined,
+            symbol: undefined,
+            quote: {
+                EUR: {
+                    price: 1
+                }
+            }
+        },
+        {
+        name: undefined,
+            symbol: undefined,
+            quote: {
+                EUR: {
+                    price: 1
+                }
+            }
+        },
+        {
+            name: undefined,
+                symbol: undefined,
+                quote: {
+                    EUR: {
+                        price: 1
+                    }
+                }
+            },
+            {
+                name: undefined,
+                    symbol: undefined,
+                    quote: {
+                        EUR: {
+                            price: undefined
+                        }
+                    }
+                },
+                {
+                    name: undefined,
+                        symbol: undefined,
+                        quote: {
+                            EUR: {
+                                price: undefined
+                            }
+                        }
+                    },
+                    {
+                        name: undefined,
+                            symbol: undefined,
+                            quote: {
+                                EUR: {
+                                    price: 1
+                                }
+                            }
+                        }, 
+                        {
+                            name: undefined,
+                                symbol: undefined,
+                                quote: {
+                                    EUR: {
+                                        price: 1
+                                    }
+                                }
+                            }
+    ]
+    }
+    
+    const [response,setResponse] = useState(cryptoTemplate)
+
+    useEffect(()=>{
+    
+    const getCoin = () => {
+        fetch("https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=1&limit=7&convert=EUR", {
+            method: "GET",
+            headers: {
+            Accept: "application/json",
+            'X-CMC_PRO_API_KEY' : '89491b09-0f1e-4585-80cf-a13097fdc682'
+            }
+        }).then(res => { 
+            let resultToPass = res.json();
+            return resultToPass;
+        })
+        .then(data => {
+            console.log(data)
+          setResponse(data);
+        })
+        .catch( error => {
+        console.error(error)
+        })   
+    }
+
+    getCoin()
+    }, [])
 
     return (
-        <Text>Ahoj</Text>
+    <View>
+        <View>
+            <Image/>
+            <Text>{response.data[0] ? response.data[0].name : '...' }</Text>
+            <Text>{response.data[0].quote.EUR.price}</Text>
+        </View>
+        <View>
+            <Image/>
+            <Text>{response.data[1].name }</Text>
+            <Text>{response.data[1].quote.EUR.price}</Text>
+        </View>
+        <View>
+            <Image/>
+            <Text>{response.data[3].name }</Text>
+            <Text>{response.data[3].quote.EUR.price}</Text>
+        </View>
+        <View>
+            <Image/>
+            <Text>{response.data[4].name}</Text>
+            <Text>{response.data[4].quote.EUR.price}</Text>
+        </View>
+        <View>
+            <Image/>
+            <Text>{response.data[6].name}</Text>
+            <Text>{response.data[6].quote.EUR.price}</Text>
+        </View>
+    </View>
     )
     } 
+
 
 export default Coin;
